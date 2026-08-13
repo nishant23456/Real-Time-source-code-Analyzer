75aff779f8b1b046c031cf3af73e129aaf9e863d
nishant23456 <nishantroutray47@gmail.com>
2026-08-13 23:26:14 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="realtime-source-code-analyzer",
+    version="0.1.0",
+    author="Nishant",
+    author_email="nishantroutray47@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    
+)
